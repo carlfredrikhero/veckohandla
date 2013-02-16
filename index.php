00c72cd2129198f780fe1c4712196843d3f6408b
carlfredrikhero <carlfredrikhero@192.168.0.91>
2013-02-16 23:16:13 +0100

    installed Slim Framework

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
+require 'vendor/autoload.php';
 
-echo 'Hello World!';
+$app = new \Slim\Slim(array(
+	'log.enabled' => true,
+));
+
+/* init secure session cookies */
+$app->add(new \Slim\Middleware\SessionCookie(array(
+    'secret' => 'HiMwyE#egRet25ifs!47+ka',
+)));
+
+$app->add(new \Slim\Middleware\ContentTypes());
+
+$app->get('/', function() use ($app){
+	echo 'Hello Slim World!';
+});
+
+$app->run();
