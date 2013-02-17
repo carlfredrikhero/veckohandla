87a647422b556b52f686f5ceaf21e9a04e6bbdae
carlfredrikhero <carlfredrikhero@192.168.0.91>
2013-02-17 02:09:25 +0100

    Changed how environment variables are read into constants.

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
 
-define('ROOT', $_SERVER['ROOT']);
 
-define("DB1_USER", $_SERVER['DB1_USER']);
-define("DB1_PASS", $_SERVER['DB1_PASS']);
-define("DB1_NAME", $_SERVER['DB1_NAME']);
-define("DB1_HOST", $_SERVER['DB1_HOST']);
+defined('ROOT') || define('ROOT', (getenv('ROOT') ? getenv('ROOT') : 'http://veckohanda.local'));
+
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'testing'));
+
+defined('DB1_USER') || define('DB1_USER', (getenv('DB1_USER') ? getenv('DB1_USER') : 'root'));
+defined('DB1_PASS') || define('DB1_PASS', (getenv('DB1_PASS') ? getenv('DB1_PASS') : 'root'));
+defined('DB1_NAME') || define('DB1_NAME', (getenv('DB1_NAME') ? getenv('DB1_NAME') : 'veckohandla'));
+defined('DB1_HOST') || define('DB1_HOST', (getenv('DB1_HOST') ? getenv('DB1_HOST') : 'localhost'));
+defined('DB1_PORT') || define('DB1_PORT', (getenv('DB1_PORT') ? getenv('DB1_PORT') : '3306'));
 
 // Make sure errors are output to the screen
-if ( $_SERVER['APPLICATION_ENV'] != 'production' ){
+if ( APPLICATION_ENV != 'production' ){
 	ini_set('display_errors', '1');
 }
